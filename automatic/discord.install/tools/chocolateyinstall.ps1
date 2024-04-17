64bc98dfc5358d83c3874973403e102c44d5c1f8
Barett <kilobyte@gmail.com>
2024-04-17 10:11:45 -0700

    add discord x64

--- automatic/discord.install/tools/chocolateyinstall.ps1
+++ automatic/discord.install/tools/chocolateyinstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName       = 'discord.install'
   fileType          = 'exe'
-  url               = 'https://dl.discordapp.net/apps/win/0.0.304/DiscordSetup.exe'
-  url64bit          = 'https://dl.discordapp.net/apps/win/0.0.304/DiscordSetup.exe'
+  url               = 'https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9039/DiscordSetup.exe'
+  url64bit          = 'https://dl.discordapp.net/distro/app/stable/win/x64/1.0.9039/DiscordSetup.exe'
 
   softwareName      = 'discord*'
 
-  checksum          = 'f4305bb77544523cea3bf7fe75a183813029c0a825dbda35fd9f7659d4550231'
+  checksum          = 'ccd1b73aa774e3deefb7672629099eec167b130521b9036b553af6e46ffdbe3f'
   checksumType      = 'sha256'
-  checksum64        = 'f4305bb77544523cea3bf7fe75a183813029c0a825dbda35fd9f7659d4550231'
+  checksum64        = '4cfeffc865e99ab59c5c9f7134bcd174cdcecac858d2e23f652be4b789a4605a'
   checksumType64    = 'sha256'
 
   silentArgs        = "-s"
