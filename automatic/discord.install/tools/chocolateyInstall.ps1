e77bacf02977c1abe83ce8d5cff6b8eb2bbcbbd8
kilobyte1024 <kilobyte@gmail.com>
2024-06-29 13:51:37 -0700

    fix var name DefaultDiscordPath in discord.install/tools/chocolateyInstall.ps1

--- automatic/discord.install/tools/chocolateyInstall.ps1
+++ automatic/discord.install/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName       = 'discord.install'
   fileType          = 'exe'
-  url               = 'https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9046/DiscordSetup.exe'
-  url64bit          = 'https://dl.discordapp.net/distro/app/stable/win/x64/1.0.9147/DiscordSetup.exe'
+  url               = 'https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9051/DiscordSetup.exe'
+  url64bit          = 'https://dl.discordapp.net/distro/app/stable/win/x64/1.0.9152/DiscordSetup.exe'
 
   softwareName      = 'discord*'
 
-  checksum          = '61a005edb0aabfeca61a2302736111ccf30d20f2f5aecd09da574de3ecb50d80'
+  checksum          = '4865f70b92896ecb15ffd09caaf579ea533978a4e4306d499dfdc2e19bd46b0d'
   checksumType      = 'sha256'
-  checksum64        = 'b515b5239d471fa26f37e816e7c7d5ed7c578348ffaf61aac2b99b4ccb1c292e'
+  checksum64        = 'd24dddbdf2970f6a51611a193bcd839faf3d7a28d4dc96adcb3c20a11424209e'
   checksumType64    = 'sha256'
 
   silentArgs        = "-s"
@@ -19,7 +19,7 @@ $packageArgs = @{
 
 # ex: C:\Users\foobar\AppData\Local\Discord\app-1.0.9147\Discord.exe
 $DiscordPath = Join-Path $Env:LOCALAPPDATA "Discord" * "Discord.exe" -Resolve
-$DiscordPresent = Test-Path -Path $DefaultDiscordPath
+$DiscordPresent = Test-Path -Path $DiscordPath
 
 if ($DiscordPresent) {
   $InstalledVersion = (Get-ItemProperty -Path $DiscordPath -ErrorAction:SilentlyContinue).VersionInfo.ProductVersion
