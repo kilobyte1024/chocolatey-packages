111724201e439c4bec0cf0d2a89689316829ab60
Appveyor Commit <kilobyte@gmail.com>
2024-11-18 01:33:29 +0000

    AU: 2 updated - discord discord.install [skip ci] https://gist.github.com/kilobyte1024/923dadc2aad7acab57ee8cbe11c2e46b/854c25e96ef112bd5a6672d0f149b491cde27d01

--- automatic/discord.install/tools/chocolateyInstall.ps1
+++ automatic/discord.install/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $packageArgs = @{
   packageName       = 'discord.install'
   fileType          = 'exe'
   url               = 'https://stable.dl2.discordapp.net/distro/app/stable/win/x86/1.0.9059/DiscordSetup.exe'
-  url64bit          = 'https://stable.dl2.discordapp.net/distro/app/stable/win/x64/1.0.9169/DiscordSetup.exe'
+  url64bit          = 'https://stable.dl2.discordapp.net/distro/app/stable/win/x64/1.0.9170/DiscordSetup.exe'
 
   softwareName      = 'discord*'
 
   checksum          = '85060117d7c75378fcbf8b3824e79549002bc5298ae46fcaa2524c83c476596a'
   checksumType      = 'sha256'
-  checksum64        = '37e9b180c65e383fc996dee7a1e597ff8bfc880ccf92e084f0d35816e34a6c1e'
+  checksum64        = '81859e4cb07878af7437ff0c442523cc3ffb4632ea8ade930bdb4d2ec868e68b'
   checksumType64    = 'sha256'
 
   silentArgs        = "-s"
