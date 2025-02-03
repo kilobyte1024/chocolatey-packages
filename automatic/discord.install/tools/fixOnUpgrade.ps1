52b8fc9561d21949d0bf3386e6e0bf775fd484e2
Charles Panchyshyn <charles.panchyshyn@gmail.com>
2025-02-03 10:09:16 -0800

    remove: copy installer.db logic
    Removed logic for detecting and handling the latest app version folder in preparation for a replacement implementation. This change simplifies the existing script to allow for a more robust solution.

--- automatic/discord.install/tools/fixOnUpgrade.ps1
+++ automatic/discord.install/tools/fixOnUpgrade.ps1
@@ -40,19 +40,3 @@ if (Test-Path $installerDbPath) {
 
 # Get the latest folder that starts with "app-"
 $appVersionFolder = Get-ChildItem -Path $discordPath | Where-Object { $_.Name -like 'app-*' } | Sort-Object -Descending | Select-Object -First 1
-
-# Check if the app version folder was found
-if ($null -ne $appVersionFolder) {
-    $sourceDbPath = Join-Path $appVersionFolder.FullName "installer.db"
-
-    # Check if the installer.db file exists in the app version folder
-    if (Test-Path $sourceDbPath) {
-        # Copy the installer.db file to the Discord folder
-        Copy-Item -Path $sourceDbPath -Destination $discordPath -Force
-        Write-Host "Copied installer.db from $($appVersionFolder.Name) to $discordPath"
-    } else {
-        Write-Host "No installer.db file found in $($appVersionFolder.Name)"
-    }
-} else {
-    Write-Host "No app version folder found."
-}
